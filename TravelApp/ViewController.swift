52f51c32b65418c4a09de16cd6d8594227fe237b
Daniil <ivanov_daniil_a@vk.com>
2023-11-01 20:35:18 +0300

    Third

--- TravelApp/ViewController.swift
+++ TravelApp/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var gjg = "fhfh"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
