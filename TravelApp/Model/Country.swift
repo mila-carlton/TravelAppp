d9d9f755942ea9235c5a545a9b43a8796ce39f77
mila-carlton <mila.carlton@mail.ru>
2023-11-02 20:11:21 +0400

    creatingModel

--- TravelApp/Model/Country.swift
+++ TravelApp/Model/Country.swift
@@ -2,36 +2,46 @@
 import Foundation
 
 
-struct Tours {
-    let name: String
-    let image: String
-    let packet: [Packet]
-}
-
-struct Packet {
-    let name: String
-    let image: String
-    let price: String
-}
-
-struct Country {
-    let name: String
-    let imageName: String
-    let tours: [Tours]
-    
-    static func getTrip() -> [Country] {
-        [
-            Country(name: "Russian"  , imageName: "#", tours: [Tours(name: "Popular Tour", image: "#", packet: [Packet(name: "Золотое кольцо", image: "#", price: "3526$")])]),
-            Country(name: "Russian"  , imageName: "#", tours: [Tours(name: "Popular Tour", image: "#", packet: [Packet(name: "Золотое кольцо", image: "#", price: "3526$")])]),
-            Country(name: "Russian"  , imageName: "#", tours: [Tours(name: "Popular Tour", image: "#", packet: [Packet(name: "Золотое кольцо", image: "#", price: "3526$")])]),
-            Country(name: "Russian"  , imageName: "#", tours: [Tours(name: "Popular Tour", image: "#", packet: [Packet(name: "Золотое кольцо", image: "#", price: "3526$")])]),
-            Country(name: "Russian"  , imageName: "#", tours: [Tours(name: "Popular Tour", image: "#", packet: [Packet(name: "Золотое кольцо", image: "#", price: "3526$")])]),
-            Country(name: "Russian"  , imageName: "#", tours: [Tours(name: "Popular Tour", image: "#", packet: [Packet(name: "Золотое кольцо", image: "#", price: "3526$")])]),
-            Country(name: "Russian"  , imageName: "#", tours: [Tours(name: "Popular Tour", image: "#", packet: [Packet(name: "Золотое кольцо", image: "#", price: "3526$")])]),
-            Country(name: "Russian"  , imageName: "#", tours: [Tours(name: "Popular Tour", image: "#", packet: [Packet(name: "Золотое кольцо", image: "#", price: "3526$")])]),
-            Country(name: "Russian"  , imageName: "#", tours: [Tours(name: "Popular Tour", image: "#", packet: [Packet(name: "Золотое кольцо", image: "#", price: "3526$")])]),
-            Country(name: "Russian"  , imageName: "#", tours: [Tours(name: "Popular Tour", image: "#", packet: [Packet(name: "Золотое кольцо", image: "#", price: "3526$")])]),
-            Country(name: "Russian"  , imageName: "#", tours: [Tours(name: "Popular Tour", image: "#", packet: [Packet(name: "Золотое кольцо", image: "#", price: "3526$")])])
-        ]
-    }
-}
+//struct Tours {
+//    let name: String
+//    let image: String
+//    let packet: [Packet]
+//}
+//
+//struct Packet {
+//    let name: String
+//    let image: String
+//    let price: String
+//}
+//
+//struct Country {
+//    let name: String
+//    let imageName: String
+//    let tours: [Tours]
+//    
+//    static func getTrip() -> [Country] {
+//        [
+//            Country(name: "Russia"  , imageName: "#", tours: [Tours(name: "Moscow Tour"), T "Saint-Petersburg Tour", "", image: "#", packet: [Packet(name: "Золотое кольцо", image: "#", price: "3526$")])]),
+//            
+//            Country(name: "Russian"  , imageName: "#", tours: [Tours(name: "Popular Tour", image: "#", packet: [Packet(name: "Золотое кольцо", image: "#", price: "3526$")])]),
+//            
+//            Country(name: "Russian"  , imageName: "#", tours: [Tours(name: "Popular Tour", image: "#", packet: [Packet(name: "Золотое кольцо", image: "#", price: "3526$")])]),
+//            
+//            Country(name: "Russian"  , imageName: "#", tours: [Tours(name: "Popular Tour", image: "#", packet: [Packet(name: "Золотое кольцо", image: "#", price: "3526$")])]),
+//            
+//            Country(name: "Russian"  , imageName: "#", tours: [Tours(name: "Popular Tour", image: "#", packet: [Packet(name: "Золотое кольцо", image: "#", price: "3526$")])]),
+//            
+//            Country(name: "Russian"  , imageName: "#", tours: [Tours(name: "Popular Tour", image: "#", packet: [Packet(name: "Золотое кольцо", image: "#", price: "3526$")])]),
+//            
+//            Country(name: "Russian"  , imageName: "#", tours: [Tours(name: "Popular Tour", image: "#", packet: [Packet(name: "Золотое кольцо", image: "#", price: "3526$")])]),
+//            
+//            Country(name: "Russian"  , imageName: "#", tours: [Tours(name: "Popular Tour", image: "#", packet: [Packet(name: "Золотое кольцо", image: "#", price: "3526$")])]),
+//            
+//            Country(name: "Russian"  , imageName: "#", tours: [Tours(name: "Popular Tour", image: "#", packet: [Packet(name: "Золотое кольцо", image: "#", price: "3526$")])]),
+//            
+//            Country(name: "Russian"  , imageName: "#", tours: [Tours(name: "Popular Tour", image: "#", packet: [Packet(name: "Золотое кольцо", image: "#", price: "3526$")])]),
+//            
+//            Country(name: "Russian"  , imageName: "#", tours: [Tours(name: "Popular Tour", image: "#", packet: [Packet(name: "Золотое кольцо", image: "#", price: "3526$")])])
+//        ]
+//    }
+//}
--- TravelApp/ViewController/ViewController.swift
+++ TravelApp/ViewController/ViewController.swift
@@ -5,8 +5,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        var a = 50
     }
 
 
